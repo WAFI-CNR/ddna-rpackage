e2b5dd13c095edecb4073003b6781eccdd568e21
ant.martella <ant.martella@gmail.com>
2019-10-17 17:39:02 +0200

    Added threshold to LCS plot

--- R/lcs_plot.R
+++ R/lcs_plot.R
@@ -17,10 +17,38 @@ lcs_plot <- function(df){
   longest_common_subsequence(df)
   # carico i dati
   filename <- paste0(getwd(), "/glcr_cache.mat", collapse = NULL)
+  filename <- "D:/Google Drive/Europee2019 CNR/SISP 2019/timeline utenti/leader following/matrici/1122076402432528384_glcr_cache.mat"
   lcs1 <- read.csv(filename, header=T, sep=",")
   colnames(lcs1) <- c("Accounts", "LCS", "x", "y")
   end1 <- lcs1$Accounts[length(lcs1$Accounts)]
   LCS_max <- max(lcs1$LCS)
+
+  decision_function <- function(lcs1, w_size = 10){
+    lunghezza <- length(lcs1$LCS)
+    w_size <- w_size
+    X_avg_diff = diff(running_mean(lcs1, w_size))
+    i = 0
+    max_index = lunghezza - w_size
+    for (j in 1:max_index){
+      print(j)
+      print(paste(X_avg_diff[j], mean(X_avg_diff[j:j+w_size])))
+      if(X_avg_diff[j] < mean(X_avg_diff[j:j+w_size])) break
+      i <- i+1
+    }
+    print(i)
+    t = which.max(X_avg_diff[i:length(X_avg_diff)])
+    print(t)
+    return(t + i)
+
+  }
+
+  running_mean <- function(x, winsize){
+    run_mean_vector <- numeric()
+    for(i in 1:length(x$LCS)){
+      run_mean_vector[i] <- round(mean(x$LCS[max(1, i):min(i + winsize, length(x$LCS))]),2)
+    }
+  return(run_mean_vector)
+  }
 
   # plot lineare
   lcs_plot1_lin <- ggplot(lcs1, aes(Accounts, LCS)) +
@@ -28,9 +56,10 @@ lcs_plot <- function(df){
     geom_point(color="dodgerblue3", shape=1, size=5, alpha=0.25) +
     scale_x_continuous(breaks = pretty_breaks(n = 5), limits = c(0, end1)) +
     scale_y_continuous(limits = c(0, LCS_max)) +
-    theme_bw(base_family = "Helvetica") +
+    #theme_bw(base_family = "Helvetica") +
     theme(axis.text.x = element_text(size=14), axis.title.x = element_text(size=18, vjust=-0.75), axis.text.y = element_text(size=14), axis.title.y = element_text(size=18, vjust=0.30)) +
-    theme(plot.title = element_text(lineheight=.8, face="bold", size=18), legend.text = element_text(size=14), legend.title = element_text(size=18), legend.key.height=unit(2,"line"), legend.key.width=unit(2,"line"), legend.key.size = unit(1.5, "lines"))
+    theme(plot.title = element_text(lineheight=.8, face="bold", size=18), legend.text = element_text(size=14), legend.title = element_text(size=18), legend.key.height=unit(2,"line"), legend.key.width=unit(2,"line"), legend.key.size = unit(1.5, "lines")) +
+    geom_vline(xintercept = decision_function(lcs1), color="orangered", linetype="dashed", size = 1.5)
 
   # preparo breaks e minor breaks per il plot log-log
   breaks <- 10^(-10:10)
@@ -45,7 +74,8 @@ lcs_plot <- function(df){
     annotation_logticks(sides='trbl') +
     #xlab("") +
     #ylab("") +
-    theme_bw(base_family = "Helvetica") +
+    #theme_bw(base_family = "Helvetica") +
+    geom_vline(xintercept = decision_function(lcs1), color="orangered", linetype="dashed", size = 1.5) +
     theme(axis.text.x = element_text(size=14), axis.title.x = element_text(size=18, vjust=-0.75), axis.text.y = element_text(size=14), axis.title.y = element_text(size=18, vjust=0.30)) +
     theme(plot.title = element_text(lineheight=.8, face="bold", size=18), legend.text = element_text(size=14), legend.title = element_text(size=18), legend.key.height=unit(2,"line"), legend.key.width=unit(2,"line"), legend.key.size = unit(1.5, "lines"))
     #theme(plot.margin = unit(c(0,0,0,0), "cm"))
